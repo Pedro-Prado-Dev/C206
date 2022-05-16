123fd7f33eb1813ae9adf4411b2f3428ab2554b1
Pedro-Prado-coder <ph.prado@gec.inatel.br>
2022-05-16 19:44:57 -0300

    Exercicio 2 sobre excecoes

---
+++ Exercicio_excecoes2/src/main/java/Conta.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Conta {
+
+    private float saldo;
+    private float limite;
+    private Set<Cliente> clientes = new HashSet<>();
+
+
+    public Conta(float saldo, float limite) {
+        this.saldo = saldo;
+        this.limite = limite;
+    }
+
+    public void addCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+    public void sacar(float quantia) throws SaldoInsuficienteException {
+
+        if(quantia <= saldo) {
+            this.saldo -= quantia;
+        }else {
+            throw new SaldoInsuficienteException("Saldo Indisponivrl !!!");
+        }
+
+    }
+
+    public void mostraInfo() {
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Limite: " + limite);
+
+
+        for (Cliente cliente : clientes) {
+            try {
+                System.out.println("Nome do cliente: " + cliente.getNome());
+                System.out.println("CPF do cliente: " + cliente.getCpf());
+            } catch (NullPointerException e) {
+                System.out.println("Nao existe cliente nessa posicao");
+            }
+        }
+    }
+}
