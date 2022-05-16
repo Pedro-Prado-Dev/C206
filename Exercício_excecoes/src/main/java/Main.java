a7d6d6e18fc11d3eb88fba5945e666f968e3a4bd
Pedro-Prado-coder <ph.prado@gec.inatel.br>
2022-05-16 19:43:45 -0300

    Exercicio 1 sobre excecoes

--- Exercício_excecoes/src/main/java/Main.java
+++ Exercício_excecoes/src/main/java/Main.java
@@ -1,8 +1,6 @@
-import java.util.HashSet;
-import java.util.Set;
-
 public class Main
 {
+
     public static void main(String[] args) {
         Cliente c1 = new Cliente("Pedro",123456789);
         Cliente c2 = new Cliente("Chris",12345698);
