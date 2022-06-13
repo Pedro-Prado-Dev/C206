0e631181935c241801b993e1633c667974ebfe2e
Pedro-Prado-coder <ph.prado@gec.inatel.br>
2022-06-13 18:07:21 -0300

    Projeto Orintada Objeto com Banco de Dados

--- Projeto_POO_BD/Clinica.sql
+++ Projeto_POO_BD/Clinica.sql
@@ -14,9 +14,13 @@ idade int not null,
 cpf varchar(45) not null
 );
 
-create table Consulta(
+create table Medico(
 id int auto_increment primary key,
-datad varchar(45) not null
+nome varchar(45) not null,
+idade int not null,
+especializacao varchar(45) not null,
+cpf varchar(45) not null,
+crm varchar(45) not null
 );
 
 create table Paciente(
@@ -24,43 +28,38 @@ id int auto_increment primary key,
 nome varchar(45) not null,
 idade int not null,
 cpf varchar(45) not null,
-idConsulta int,
-constraint fk3
-foreign key (idConsulta)
-references Consulta(id)
+idMedico int,
+constraint fk1
+foreign key (idMedico)
+references Medico(id)
 ON DELETE CASCADE 
 ON UPDATE CASCADE
 ); 
+
+create table Consulta(
+id int auto_increment primary key,
+datad varchar(45) not null,
+idPaciente int,
+constraint fk2
+foreign key (idPaciente)
+references Paciente(id)
+ON DELETE CASCADE 
+ON UPDATE CASCADE
+);
 
 create table Cirurgia(
 id int auto_increment primary key,
 datad varchar(45) not null,
 idPaciente int,
-constraint fk4
+idMedico int,
+constraint fk3
 foreign key (idPaciente)
 references Paciente (id)
 ON DELETE CASCADE 
-ON UPDATE CASCADE
-);
-
-create table Medico(
-id int auto_increment,
-nome varchar(45) not null,
-idade int not null,
-especializacao varchar(45) not null,
-cpf varchar(45) not null,
-crm varchar(45) not null,
-idPaciente int,
-idCirurgia int,
-primary key(id), 
-constraint fk1 
-foreign key (idPaciente)
-references Paciente(id)
-ON DELETE CASCADE 
 ON UPDATE CASCADE,
-constraint fk2 
-foreign key (idCirurgia) 
-references Cirurgia(id)
+constraint fk4
+foreign key (idMedico)
+references Medico(id)
 ON DELETE CASCADE 
 ON UPDATE CASCADE
 );
@@ -81,13 +80,13 @@ ON DELETE CASCADE
 ON UPDATE CASCADE
 );
 
-insert into Consulta values (1, '22.06.2022'); 
+insert into Medico values (1, 'Jorge', 50, 'Pediatria', '14952079600', '3711');
 
 insert into Paciente values (1, 'Matheus', 10, '14789632500', 1); 
 
-insert into Cirurgia values (1, '10.07.2022', 1); 
+insert into Consulta values (1, '22.06.2022', 1); 
 
-insert into Medico values (1, 'Jorge', 50, 'Pediatria', '14952079600', '3711', 1, 1);
+insert into Cirurgia values (1, '10.07.2022', 1, 1); 
 
 select * from Medico;
 
